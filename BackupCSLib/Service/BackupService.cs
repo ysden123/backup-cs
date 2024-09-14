d2d32d89fe4ae1a478e3b46b5e4894254457b949
Yuriy Stul <ysden123@gmail.com>
2024-09-14 20:18:54 +0300

    V 1.0.2 duration format; destination directories creation bug fixed

--- BackupCSLib/Service/BackupService.cs
+++ BackupCSLib/Service/BackupService.cs
@@ -24,86 +24,94 @@ namespace BackupCSLib.Service
 
         private void MakeCopy(FolderConfig folder)
         {
-            //Console.WriteLine($"Create backup for project {folder.Name}");
-            TheActions.AddTotalLine("\n");
-            TheActions.SetProjectName(folder.Name!);
-            TheActions.AddTotalLine($"Processing {folder.Name!}");
-            TheActions.InitDuraion();
-            TheActions.InitProgressBar(0, 100);
-            TheActions.SetProgressBar(0);
-            var start = DateTime.Now;
-
-            TheActions.SetActionName("Remove previous back folder");
-            BackupCleaner.RemoveBackupFolder(folder, TheActions);
-
-            var dstFolderPath = VersionService.BuildOutputDirectoryName(folder);
-
-            var startPointOfFileName = folder.Source!.Length;
-
-            TheActions.SetActionName("Getting file list...");
-            TheActions.AddTotalLine("Getting file list...");
-            //Build source file list
-            var filesToCopy = new Dictionary<string, string>(); // Key - src file name, value - destination file name
-            foreach (var file in Directory.GetFiles(folder.Source!, "*", SearchOption.AllDirectories))
+            try
             {
-                var toSkip = false;
-                foreach (var skipFolder in folder.DirectoriesToSkip!)
+                //Console.WriteLine($"Create backup for project {folder.Name}");
+                TheActions.AddTotalLine("\n");
+                TheActions.SetProjectName(folder.Name!);
+                TheActions.AddTotalLine($"Processing {folder.Name!}");
+                TheActions.InitDuraion();
+                TheActions.InitProgressBar(0, 100);
+                TheActions.SetProgressBar(0);
+                var start = DateTime.Now;
+
+                TheActions.SetActionName("Remove previous back folder");
+                BackupCleaner.RemoveBackupFolder(folder, TheActions);
+
+                var dstFolderPath = VersionService.BuildOutputDirectoryName(folder);
+
+                var startPointOfFileName = folder.Source!.Length;
+
+                TheActions.SetActionName("Getting file list...");
+                TheActions.AddTotalLine("Getting file list...");
+                //Build source file list
+                var filesToCopy = new Dictionary<string, string>(); // Key - src file name, value - destination file name
+                foreach (var file in Directory.GetFiles(folder.Source!, "*", SearchOption.AllDirectories))
                 {
-                    if (file.StartsWith(skipFolder))
+                    var toSkip = false;
+                    foreach (var skipFolder in folder.DirectoriesToSkip!)
                     {
-                        toSkip = true;
-                        break;
+                        if (file.StartsWith(skipFolder))
+                        {
+                            toSkip = true;
+                            break;
+                        }
+                    }
+
+                    if (!toSkip) filesToCopy.Add(file, Path.Combine(dstFolderPath, file[startPointOfFileName..]));
+                }
+
+                TheActions.SetActionName($"Backup to {dstFolderPath}");
+                TheActions.AddTotalLine($"Backup {filesToCopy.Count} files to {dstFolderPath}");
+                TheActions.InitProgressBar(0, filesToCopy.Count);
+                TheActions.SetProgressBar(0);
+                // todo remove it!!!
+                /*Console.WriteLine("File list to copy:");
+                foreach (var entry in filesToCopy)
+                {
+                    Console.WriteLine($"{entry.Key} -> {entry.Value}");
+                }*/
+                var count = 0;
+                var outputStep = 100;
+                var outputCount = 0;
+                // Copy files
+                // todo copy files
+                foreach (var entry in filesToCopy)
+                {
+                    try
+                    {
+                        var parentDir = Directory.GetParent(entry.Value);
+                        if (!parentDir!.Exists) parentDir.Create();
+                        File.Copy(entry.Key, entry.Value, true);
+                        count++;
+                        if (++outputCount >= outputStep)
+                        {
+                            TheActions.SetProgressBar(count);
+                            TheActions.SetDuration(BuildDurationText(start));
+                            outputCount = 0;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        TheActions.AddTotalLine($"!!!ERROR: {ex.Message}");
                     }
                 }
-
-                if (!toSkip) filesToCopy.Add(file, Path.Combine(dstFolderPath, file[startPointOfFileName..]));
+                TheActions.SetProgressBar(count);
+                TheActions.AddTotalLine($"Total copied {count} files in {BuildDurationText(start)}");
             }
-
-            TheActions.SetActionName($"Backup to {dstFolderPath}");
-            TheActions.AddTotalLine($"Backup to {dstFolderPath}");
-            TheActions.InitProgressBar(0, filesToCopy.Count);
-            TheActions.SetProgressBar(0);
-            // todo remove it!!!
-            /*Console.WriteLine("File list to copy:");
-            foreach (var entry in filesToCopy)
+            catch (Exception e)
             {
-                Console.WriteLine($"{entry.Key} -> {entry.Value}");
-            }*/
-            var count = 0;
-            var outputStep = 100;
-            var outputCount = 0;
-            // Copy files
-            // todo copy files
-            foreach (var entry in filesToCopy)
-            {
-                try
-                {
-                    var parentDir = Directory.GetParent(entry.Value);
-                    if (!parentDir!.Exists) parentDir.Create();
-                    File.Copy(entry.Key, entry.Value, true);
-                    count++;
-                    if (++outputCount >= outputStep)
-                    {
-                        TheActions.SetProgressBar(count);
-                        TheActions.SetDuration(BuildDurationText(start));
-                        outputCount = 0;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    TheActions.AddTotalLine(ex.Message);
-                }
+                TheActions.AddTotalLine($"ERROR: {e.Message}");
             }
-            TheActions.SetProgressBar(count);
-            TheActions.AddTotalLine($"Total copied {count} files in {BuildDurationText(start)}");
         }
 
         private static string BuildDurationText(DateTime start)
         {
             var now = DateTime.Now;
-            var hours = (int)(now - start).TotalHours;
-            var minutes = (int)(now - start).TotalMinutes;
-            var seconds = (int)(now - start).TotalSeconds;
+            var dif = now - start;
+            var hours = dif.Hours;
+            var minutes = dif.Minutes;
+            var seconds = dif.Seconds;
             return $"{hours}:{minutes}:{seconds}";
         }
     }
